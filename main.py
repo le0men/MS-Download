c1a110dd2f79f2af4927d6eaf1d1e6c60766dd08
le0men <leolemon999@gmail.com>
2023-09-05 12:26:04 -0400

    Getting Started
    - Created Requirements.txt
    - Created main.py
    -Set up github and vscode

---
+++ main.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+response = requests.get("https://en.wikipedia.org/wiki/Web_scraping")
+bs = BeautifulSoup(response.text,"lxml")
+print(bs.find("p").text)
